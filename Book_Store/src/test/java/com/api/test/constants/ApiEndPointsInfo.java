910171fd90d60e1fd4bba702356338f1a712797e
Agowtham1 <gowthaman23041991@gmail.com>
2025-09-12 14:46:21 +0530

    Added base files & test files

---
+++ Book_Store/src/test/java/com/api/test/constants/ApiEndPointsInfo.java
@@ -0,0 +1,10 @@
+package com.api.test.constants;
+
+public class ApiEndPointsInfo {
+    public static final String SIGN_UP = "signup";
+    public static final String LOGIN = "login";
+    public static final String ADD_NEW_BOOK = "books/";
+    public static final String GET_ALL_BOOKS = "books/";
+    public static final String GET_BOOK_ID = "books/%s";
+    public static final String HEALTH_CHECK = "health";
+}
